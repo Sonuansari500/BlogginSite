15e83e23c3c5b066bf7bdf31c65016098936225d
iqbal <sonuansari500@gmail.com>
2023-03-10 22:57:16 +0530

    intial setup done

---
+++ Backend/bloggingsite/src/main/java/com/bloggingsite/BloggingsiteApplication.java
@@ -0,0 +1,13 @@
+package com.bloggingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingsiteApplication.class, args);
+	}
+
+}
---
+++ Backend/bloggingsite/src/test/java/com/bloggingsite/BloggingsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloggingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
