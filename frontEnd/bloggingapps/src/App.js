d33b0f31989f05d569b60fad0f2006e9687e17a6
iqbal <sonuansari500@gmail.com>
2023-03-11 20:33:21 +0530

    added carousel on home page

--- frontEnd/bloggingapps/src/App.js
+++ frontEnd/bloggingapps/src/App.js
@@ -1,7 +1,7 @@
 import Header from "./common/Header";
 import Navbar from "./common/Navbar";
 import Footer from "./common/Footer";
-import Home from "./component/Home";
+import Home from "./page/Home"
 import logo from "./Images/islamic-logo.png";
 import Carousel from "./component/Carousel";
 
@@ -10,7 +10,6 @@ function App() {
     <div className="main">
       <Header />
       <Navbar logo="logo" />
-      <Carousel/>
       <Home />
       <Footer />
     </div>
---
+++ frontEnd/bloggingapps/src/page/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Carousel from '../component/Carousel'
+
+export default function Home() {
+  return (
+    <>
+     <Carousel/>
+     other comming soon
+     </>
+   
+    
+  )
+}
